255b3058bcb12b9e8b908111b8b91fce0898ceb3
CarlBetsa <carl.betsa@ges.inatel.br>
2024-12-18 12:14:33 -0300

    Garantindo conexão com a Titan

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
-/* VIDs e PIDs apenas ilustrativos */
-const SATURNO_VID = 1241;
-const VID2 = 9610;
+const SATURNO_VID = 5824;
 
-const SUPERNOVA_PID = 22;
-const TITAN_PID = 64560;
+const SUPERNOVA_PID = 1234;
+const TITAN_PID = 1508;
 
 let selectedDevice = null;
 
 function getUsbDevice() {
     if ('usb' in navigator) {
-        navigator.usb.requestDevice({ filters: [] })
+        navigator.usb.requestDevice({ filters: [{vendorId: 5824}] })
             .then(device => {
-                if (device.vendorId === SATURNO_VID || device.vendorId === VID2) {
-                    const usbDeviceDiv = document.getElementById('device');
-                    const usbDevice = document.getElementById('usbDevice');
-                    const selectButton = document.getElementById('select');
+                const usbDeviceDiv = document.getElementById('device');
+                const usbDevice = document.getElementById('usbDevice');
+                const selectButton = document.getElementById('select');
 
-                    if (device.productId === SUPERNOVA_PID) {
-                        usbDevice.textContent = 'Controladora Supernova';
-                        selectedDevice = 'supernova';
-                    } else if (device.productId === TITAN_PID) {
-                        usbDevice.textContent = 'Controladora Titan';
-                        selectedDevice = 'titan';
-                    } else {
-                        alert('Dispositivo Saturno não cadastrado no editor.');
-                        return;
-                    }
-                    selectButton.textContent = "Selecionar outro controlador";
-                    usbDeviceDiv.style.display = 'block';
+                if (device.productId === SUPERNOVA_PID) {
+                    usbDevice.textContent = 'Controladora Supernova';
+                    selectedDevice = 'supernova';
+                } else if (device.productId === TITAN_PID) {
+                    usbDevice.textContent = 'Controladora Titan';
+                    selectedDevice = 'titan';
                 } else {
-                    alert('Dispositivo selecionado não é um controlador Saturno.');
+                    alert('Dispositivo Saturno não cadastrado no editor.');
+                    return;
                 }
+                selectButton.textContent = "Selecionar outro controlador";
+                usbDeviceDiv.style.display = 'block';
             })
             .catch(error => {
                 if (error.name === 'NotFoundError' && error.message.includes('No device selected')) {
@@ -46,8 +40,8 @@ function getUsbDevice() {
 
 function confirmSelection() {
     if (selectedDevice === 'supernova') {
-        window.location.href = 'templates/supernova.html'; // Redireciona para supernova.html
+        window.location.href = 'templates/supernova.html';
     } else if (selectedDevice === 'titan') {
-        window.location.href = 'templates/titan.html'; // Redireciona para titan.html
+        window.location.href = 'templates/titan.html';
     }
 }
