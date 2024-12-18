92e7a5ef0abd98760097c0027410a1af15e6f5b5
CarlBetsa <carl.betsa@ges.inatel.br>
2024-12-18 14:15:58 -0300

    Corrigindo erro PC sem USB

--- script.js
+++ script.js
@@ -6,36 +6,33 @@ const TITAN_PID = 1508;
 let selectedDevice = null;
 
 function getUsbDevice() {
-    if ('usb' in navigator) {
-        navigator.usb.requestDevice({ filters: [{vendorId: 5824}] })
-            .then(device => {
-                const usbDeviceDiv = document.getElementById('device');
-                const usbDevice = document.getElementById('usbDevice');
-                const selectButton = document.getElementById('select');
+    navigator.usb.requestDevice({ filters: [{ vendorId: 5824 }] })
+        .then(device => {
+            const usbDeviceDiv = document.getElementById('device');
+            const usbDevice = document.getElementById('usbDevice');
+            const selectButton = document.getElementById('select');
 
-                if (device.productId === SUPERNOVA_PID) {
-                    usbDevice.textContent = 'Controladora Supernova';
-                    selectedDevice = 'supernova';
-                } else if (device.productId === TITAN_PID) {
-                    usbDevice.textContent = 'Controladora Titan';
-                    selectedDevice = 'titan';
-                } else {
-                    alert('Dispositivo Saturno não cadastrado no editor.');
-                    return;
-                }
-                selectButton.textContent = "Selecionar outro controlador";
-                usbDeviceDiv.style.display = 'block';
-            })
-            .catch(error => {
-                if (error.name === 'NotFoundError' && error.message.includes('No device selected')) {
-                    alert('Selecione seu controlador Saturno para continuar.');
-                } else {
-                    alert('Erro ao acessar dispositivos USB: ' + error);
-                }
-            });
-    } else {
-        alert('Navegador não suportado, por favor utilize navegadores como Chrome ou Edge.');
-    }
+            if (device.productId === SUPERNOVA_PID) {
+                usbDevice.textContent = 'Controladora Supernova';
+                selectedDevice = 'supernova';
+            } else if (device.productId === TITAN_PID) {
+                usbDevice.textContent = 'Controladora Titan';
+                selectedDevice = 'titan';
+            } else {
+                alert('Dispositivo Saturno não cadastrado no editor.');
+                return;
+            }
+            selectButton.textContent = "Selecionar outro controlador";
+            usbDeviceDiv.style.display = 'block';
+        })
+        .catch(error => {
+            if (error.name === 'NotFoundError' && error.message.includes('No device selected')) {
+                alert('Selecione seu controlador Saturno para continuar.');
+            } else {
+                alert('Erro ao acessar dispositivos USB: ' + error);
+            }
+        });
+
 }
 
 function confirmSelection() {
