6d7e46014de832630c0d056f073e957baa782944
CarlBetsa <carl.betsa@ges.inatel.br>
2025-01-13 15:57:38 -0300

    Avançando tabela MIDI

--- script.js
+++ script.js
@@ -89,6 +89,7 @@ function createBankPatches(letter, index) {
             selectedPatchType.textContent = `(${type})`;
 
             createLoopTable(patchId, index);
+            createMidiTable(patchId, index);
         });
 
         inputElement.addEventListener('input', () => {
@@ -169,7 +170,7 @@ function createLoopTable(patchId, index) {
         ? 'rgba(83, 191, 235, 0.5)'
         : 'rgba(159, 24, 253, 0.5)';
     loopTableFull.style.fontWeight = 'bold';
-    loopTableFull.style.padding = '20px';
+    loopTableFull.style.padding = '10px';
     loopTableFull.style.marginLeft = '20vh';
     loopTableFull.style.borderRadius = '10px';
     loopTableFull.style.width = '50vh';
@@ -178,7 +179,7 @@ function createLoopTable(patchId, index) {
     loopTable.style.display = 'grid';
     loopTable.style.gridTemplateColumns = '1fr 1fr'; // Duas colunas
     loopTable.style.columnGap = '80px';
-    loopTable.style.rowGap = '20px';
+    loopTable.style.rowGap = '10px';
     
 
     // Cria as linhas da tabela de loops
@@ -211,7 +212,7 @@ function createLoopTable(patchId, index) {
 let currentOpenPresetOptions = null;
 function createPresetOptions(patchTypeButton, letter, number, index) {
     const presetOptions = document.createElement('div');
-    presetOptions.className = 'preset-options';
+    presetOptions.className = 'popup-options';
 
     ['Preset', 'Action', 'Toggle Action', 'Momentary', 'Tap', 'Tuner'].forEach((preset) => {
         const optionButton = document.createElement('button');
@@ -294,6 +295,332 @@ function saveLoopStates(index, states) {
 function loadLoopStates(index) {
     const savedStates = localStorage.getItem(`${index}_loops`);
     return savedStates ? JSON.parse(savedStates) : Array(8).fill(false);
+}
+
+// Cria a tabela MIDI
+let currentOpenMidiPopup = null;
+function createMidiTable(patchId, index) {
+    const midiTable = document.getElementById('midi-table');
+    const midiTableFull = document.getElementById('table-2');
+
+    midiTableFull.style.display = 'grid';
+    midiTableFull.style.backgroundColor = index % 2 === 0
+        ? 'rgba(83, 191, 235, 0.5)'
+        : 'rgba(159, 24, 253, 0.5)';
+    midiTableFull.style.fontWeight = 'bold';
+    midiTableFull.style.padding = '10px';
+    midiTableFull.style.marginLeft = '20vh';
+    midiTableFull.style.marginTop = '5vh';
+    midiTableFull.style.borderRadius = '10px';
+    midiTableFull.style.width = '50vh';
+    
+    // Limpa a tabela MIDI
+    midiTable.innerHTML = '';
+    midiTable.style.display = 'grid';
+    midiTable.style.gridTemplateColumns = '1fr 1fr'; // Divide em duas colunas
+    midiTable.style.columnGap = '30px';
+    midiTable.style.rowGap = '10px';
+
+    const states = loadMidiStates(patchId);
+
+    // Cria as linhas da tabela MIDI
+    for (let i = 0; i < 10; i++) {
+        const midiRow = document.createElement('div');
+        midiRow.className = 'midi-row';
+        midiRow.style.display = 'flex';
+        midiRow.style.justifyContent = 'space-between';
+        midiRow.style.alignItems = 'center';
+    
+        // Cria o botão principal MIDI
+        const midiButton = document.createElement('span');
+        midiButton.textContent = states[i]?.type || 'OFF';
+        midiButton.className = 'toggle';
+        midiButton.style.color = midiButton.textContent === 'OFF' ? 'red' : 'lime';
+    
+        // Cria o popup ao clicar
+        midiButton.addEventListener('click', (e) => {
+            e.stopPropagation();
+            createMidiPopup(midiButton, patchId, i);
+        });
+    
+        // Adiciona o botão MIDI na linha
+        midiRow.appendChild(midiButton);
+    
+        if (states[i]?.type && states[i].type !== 'OFF') {
+            const values = states[i]?.values || [0, 1];
+
+            //cria botões secundarios
+            values.forEach((value, idx) => {
+                const detailButton = createDetailButton(value, idx, patchId, i);
+                detailButton.textContent = value.toString();
+                detailButton.className = 'midi-detail';
+                detailButton.style.marginLeft = '10px';
+                detailButton.style.padding = '5px 10px';
+                detailButton.style.borderRadius = '5px';
+                detailButton.style.cursor = 'pointer';
+        
+                detailButton.addEventListener('click', (e) => {
+                    e.stopPropagation();
+                    const rangeStart = idx === 0 ? 1 : 0;
+                    const rangeEnd = idx === 0 ? 16 : 127;
+                    createValuePopup(detailButton, rangeStart, rangeEnd, (selectedValue) => {
+                        detailButton.textContent = selectedValue.toString(); // Atualiza no front
+                        
+                        // Salva no banco
+                        states[i].values[idx] = selectedValue;
+                        localStorage.setItem(`${patchId}_midi`, JSON.stringify(states));
+                    });
+                });
+
+                midiButton.insertAdjacentElement('afterend', detailButton);
+            });
+        }        
+        
+        // Adiciona a linha na tabela MIDI
+        midiTable.appendChild(midiRow);
+    }
+    
+}
+
+// Cria o popup do comando
+function createMidiPopup(midiButton, patchId, index) {
+
+    // Fecha outros popups
+    if (currentOpenMidiPopup) {
+        currentOpenMidiPopup.style.display = 'none';
+    }
+
+    // Cria o popup
+    let midiPopup = midiButton.querySelector('.popup-options');
+    if (!midiPopup) {
+        midiPopup = document.createElement('div');
+        midiPopup.className = 'popup-options';
+
+        const rect = midiButton.getBoundingClientRect();
+        const popupHeight = 200;
+
+        let top = rect.bottom;
+        let left = rect.left;
+
+        // Impede do popup "sair" da janela
+        if (top + popupHeight > window.innerHeight) {
+            top = Math.max(rect.top - popupHeight, 0);
+        }
+
+        midiPopup.style.top = `${top}px`;
+        midiPopup.style.left = `${left}px`;
+
+        midiPopup.style.maxWidth = '100px';
+        midiPopup.style.maxHeight = '200px';
+        midiPopup.style.overflowY = 'auto';
+        midiPopup.style.overflowX = 'hidden';
+        midiPopup.style.scrollbarWidth = 'none';
+
+        // Adiciona as opções OFF e PC no popup
+        ['OFF', 'PC'].forEach((option) => {
+            const optionButton = document.createElement('button');
+            optionButton.textContent = option;
+            optionButton.style.marginRight = '10px';
+            optionButton.style.width = '80px';
+            optionButton.addEventListener('click', () => {
+                handleMidiSelection(option, midiButton, patchId, index);
+                midiPopup.style.display = 'none';
+            });
+            midiPopup.appendChild(optionButton);
+        });
+
+        // Adiciona as opções CCs no popup
+        for (let i = 0; i <= 127; i++) {
+            const optionButton = document.createElement('button');
+            let option = `CC${i}`;
+            optionButton.textContent = option;
+            optionButton.style.marginRight = '10px';
+            optionButton.style.width = '80px';
+            optionButton.addEventListener('click', () => {
+                handleMidiSelection(option, midiButton, patchId, index);
+                midiPopup.style.display = 'none';
+            });
+            midiPopup.appendChild(optionButton);
+        }
+
+        midiButton.appendChild(midiPopup);
+    }
+
+    // MOstra o popup
+    midiPopup.style.display = 'block';
+    currentOpenMidiPopup = midiPopup;
+
+    // Fecha o popup se clicar fora
+    document.addEventListener(
+        'click',
+        (e) => {
+            if (!midiPopup.contains(e.target)) {
+                midiPopup.style.display = 'none';
+                currentOpenMidiPopup = null;
+            }
+        },
+        { once: true }
+    );
+}
+
+// Cria botão secundario
+function createDetailButton(initialValue, index, patchId, midiIndex) {
+    const detailButton = document.createElement('button');
+    detailButton.textContent = initialValue.toString();
+    detailButton.className = 'midi-detail';
+    detailButton.style.marginLeft = '10px';
+    detailButton.style.padding = '5px 10px';
+    detailButton.style.borderRadius = '5px';
+    detailButton.style.cursor = 'pointer';
+
+    // Lida com o clique no botão
+    detailButton.addEventListener('click', (e) => {
+        e.stopPropagation(); // Impede bug ao trocar comando
+        const rangeStart = index === 0 ? 0 : 1;
+        const rangeEnd = index === 0 ? 127 : 16;
+        createValuePopup(detailButton, rangeStart, rangeEnd, (selectedValue) => {
+            detailButton.textContent = selectedValue.toString();
+
+            const states = loadMidiStates(patchId);
+            states[midiIndex].values[index] = selectedValue; // Atualiza no front
+            localStorage.setItem(`${patchId}_midi`, JSON.stringify(states)); // Salva no banco
+        });
+    });
+
+    return detailButton;
+}
+
+
+// Lida com a seleção de comando
+function handleMidiSelection(type, midiButton, patchId, index) {
+    const parentRow = midiButton.parentElement;
+
+    // Atualiza o botão principal
+    midiButton.textContent = type;
+    midiButton.style.color = type === 'OFF' ? 'red' : 'lime';
+
+    // Limpa botões secundarios
+    const existingDetailButtons = parentRow.querySelectorAll('.midi-detail');
+    existingDetailButtons.forEach((btn) => btn.remove());
+
+    // Salva e sai se escolher OFF
+    if (type === 'OFF') {
+        localStorage.setItem(`${patchId}_midi`, JSON.stringify({ type: 'OFF', values: [] }));
+        return;
+    }
+
+    // Cria botões secundarios
+    for (let i = 0; i < 2; i++) {
+        const detailButton = createDetailButton(0, i, patchId, index);
+        parentRow.appendChild(detailButton);
+    }
+
+    // Salva
+    const midiData = { type: type, values: [0, 1] };
+    const states = loadMidiStates(patchId);
+    states[index] = midiData;
+    localStorage.setItem(`${patchId}_midi`, JSON.stringify(states));
+}
+
+// Cria popup pro valor e canal
+function createValuePopup(detailButton, rangeStart, rangeEnd, onSelectCallback) {
+
+    // Fecha popups abertos
+    const existingPopup = document.querySelector('.value-popup');
+    if (existingPopup) existingPopup.remove();
+
+    // Cria o popup
+    const valuePopup = document.createElement('div');
+    valuePopup.className = 'value-popup';
+    valuePopup.style.position = 'absolute';
+    valuePopup.style.backgroundColor = '#242424';
+    valuePopup.style.borderRadius = '5px';
+    valuePopup.style.maxHeight = '200px';
+    valuePopup.style.overflowY = 'auto';
+    valuePopup.style.scrollbarWidth = 'none';
+    valuePopup.style.width = '60px';
+    valuePopup.style.textAlign = 'center';
+
+    // Impede popup de "sair" da tela
+    const rect = detailButton.getBoundingClientRect();
+    const popupHeight = 200;
+    const popupWidth = 60;
+
+    let top = rect.bottom;
+    let left = rect.left;
+
+    if (top + popupHeight > window.innerHeight) {
+        top = Math.max(rect.top - popupHeight, 0); 
+    }
+    
+    if (left + popupWidth > window.innerWidth) {
+        left = Math.max(window.innerWidth - popupWidth, 0);
+    }
+
+    valuePopup.style.top = `${top}px`;
+    valuePopup.style.left = `${left}px`;
+    
+    // Adiciona numeros ao popup
+    for (let i = rangeStart; i <= rangeEnd; i++) {
+        const valueButton = document.createElement('button');
+        valueButton.textContent = i.toString();
+        valueButton.style.display = 'block';
+        valueButton.style.width = '100%';
+        valueButton.style.marginBottom = '5px';
+        valueButton.style.padding = '5px';
+        valueButton.style.cursor = 'pointer';
+
+        // Salva e esconde o popup
+        valueButton.addEventListener('click', () => {
+            onSelectCallback(i);
+            valuePopup.remove();
+        });
+
+        valuePopup.appendChild(valueButton);
+    }
+
+    // Adiciona EXP1 e EXP2 no primeiro botão
+    if (rangeStart === 0) {
+        ['EXP1', 'EXP2'].forEach((option) => {
+            const valueButton = document.createElement('button');
+            valueButton.textContent = option;
+            valueButton.style.display = 'block';
+            valueButton.style.width = '100%';
+            valueButton.style.marginBottom = '5px';
+            valueButton.style.padding = '5px';
+            valueButton.style.cursor = 'pointer';
+    
+            // Salva e esconde o popup
+            valueButton.addEventListener('click', () => {
+                onSelectCallback(option);
+                valuePopup.remove();
+            });
+    
+            valuePopup.appendChild(valueButton);
+        });
+    }
+
+    // Acopla o popup ao site
+    document.body.appendChild(valuePopup);
+
+    // Fecha o popup ao clicar fora
+    document.addEventListener(
+        'click',
+        (e) => {
+            if (!valuePopup.contains(e.target)) {
+                valuePopup.remove();
+            }
+        },
+        { once: true }
+    );
+}
+
+// Salva do tabela no banco
+function loadMidiStates(patchId) {
+    const savedStates = localStorage.getItem(`${patchId}_midi`);
+    return savedStates
+        ? JSON.parse(savedStates)
+        : Array(10).fill({ type: 'OFF', values: [] });
 }
 
 initializeSite();
