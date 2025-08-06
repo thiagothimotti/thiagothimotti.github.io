c020a983955db4e141e1a77cde8e402f78bea160
CarlBetsa <carl.betsa@ges.inatel.br>
2025-08-06 10:44:59 -0300

    Corrigindo bug Heel e Toe

--- pedalScript.js
+++ pedalScript.js
@@ -99,11 +99,11 @@ const parameterRanges = {
     "Release": { tipo: "porcentagem", valor_inicial: 0, valor_final: 100, complemento: "%" },
     "Responce": { tipo: "porcentagem", valor_inicial: 0, valor_final: 100, complemento: "%" },
     "Heel": {
-        tipo: "lista", valores: [-12, -11, -10, -9, -8, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5,
+        tipo: "lista", valores: [-12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5,
             6, 7, 8, 9, 10, 11, 12], complemento: ""
     },
     "Toe": {
-        tipo: "lista", valores: [-12, -11, -10, -9, -8, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6,
+        tipo: "lista", valores: [-12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6,
             7, 8, 9, 10, 11, 12], complemento: ""
     },
     "Shape": { tipo: "lista", valores: ["Triangle", "Square", "Sine"], complemento: "" },
